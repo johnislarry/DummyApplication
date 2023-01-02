06accef141b166e708dc6615b852786dc3a26e44
Max Sherman <john@maxsherman.com>
2023-01-01 22:08:27 -0500

    first commit

---
+++ app/src/main/java/com/example/dummyapplication/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.example.dummyapplication.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
